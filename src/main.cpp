b3ea8ae0c15fc388ec382961cd2cd632f1a5ab21
fantasy-peak <1356346239@qq.com>
2022-04-13 16:00:41 +0900

    add folly

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,52 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
 #include <fmt/ranges.h>
-#include <spdlog/spdlog.h>
-#include <sw/redis++/async_redis++.h>
 #include <trantor/net/EventLoopThreadPool.h>
+
+#include <spdlog/spdlog.h>
+
+#include <sw/redis++/async_redis++.h>
+
 #include <nlohmann/json.hpp>
 
-int main() {
+#include <folly/Portability.h>
+#include <folly/init/Init.h>
+
+#include <folly/Singleton.h>
+#include <folly/executors/CPUThreadPoolExecutor.h>
+#include <folly/executors/GlobalExecutor.h>
+#include <folly/executors/ManualExecutor.h>
+#include <folly/experimental/ThreadWheelTimekeeperHighRes.h>
+#include <folly/experimental/coro/AsyncScope.h>
+#include <folly/experimental/coro/Baton.h>
+#include <folly/experimental/coro/BlockingWait.h>
+#include <folly/experimental/coro/Collect.h>
+#include <folly/experimental/coro/CurrentExecutor.h>
+#include <folly/experimental/coro/FutureUtil.h>
+#include <folly/experimental/coro/Generator.h>
+#include <folly/experimental/coro/Mutex.h>
+#include <folly/experimental/coro/SharedMutex.h>
+#include <folly/experimental/coro/Sleep.h>
+#include <folly/experimental/coro/Task.h>
+#include <folly/experimental/coro/Timeout.h>
+#include <folly/futures/ThreadWheelTimekeeper.h>
+
+using namespace std::chrono_literals;
+
+void init(int argc, char** argv) {
+	// folly::init(&argc, &argv);
+	folly::SingletonVault::singleton()->registrationComplete();
+}
+
+auto get_id() {
+	std::stringstream ss;
+	ss << std::this_thread::get_id();
+	return ss.str();
+}
+
+int main(int argc, char** argv) {
+	init(argc, argv);
+	spdlog::info("main thread id: {}", get_id());
 	spdlog::info("------------------start test redis-plus-plus---------------------------");
 	sw::redis::ConnectionOptions opts;
 	opts.host = "127.0.0.1";
@@ -42,7 +82,7 @@ int main() {
 		spdlog::info("end query!!!");
 		co_return;
 	};
-	auto create_lazy_task = [&]() -> drogon::Task<void> {
+	auto create_lazy_task = [&]() -> folly::coro::Task<void> {
 		try {
 			co_await task();
 		} catch (const std::runtime_error& e) {
@@ -51,6 +91,103 @@ int main() {
 			fmt::print("{}\n", e.what());
 		}
 	};
-	drogon::sync_wait(create_lazy_task());
+	folly::coro::blockingWait(create_lazy_task());
+	spdlog::info("------------------start test folly---------------------------");
+	folly::CPUThreadPoolExecutor thread_pool{4, std::make_shared<folly::NamedThreadFactory>("TestThreadPool")};
+	std::vector<folly::Future<folly::Unit>> futures;
+	for (size_t i = 0; i < 4; ++i) {
+		futures.emplace_back(folly::via(&thread_pool, [] {
+			spdlog::info("{}", get_id());
+			std::this_thread::sleep_for(std::chrono::milliseconds(200));
+		}));
+	}
+	folly::collectAll(std::move(futures)).get();
+
+	auto make_task = [&]() -> folly::coro::Task<void> {
+		spdlog::info("start folly::coro::sleep");
+		co_await folly::coro::sleep(1s, folly::Singleton<folly::ThreadWheelTimekeeper>::get());
+		spdlog::info("end folly::coro::sleep");
+		co_return;
+	};
+	auto v = make_task().scheduleOn(&thread_pool).start();
+	std::move(v).get();
+
+	spdlog::info("start test SharedMutex");
+	folly::coro::SharedMutex mutex;
+	auto make_writer_task = [&]() -> folly::coro::Task<void> {
+		auto lock = co_await mutex.co_scoped_lock();
+	};
+	auto make_reader_task = [&]() -> folly::coro::Task<void> {
+		auto lock = co_await mutex.co_scoped_lock_shared();
+	};
+	auto w1 = make_writer_task().scheduleOn(&thread_pool).start();
+	auto r1 = make_reader_task().scheduleOn(&thread_pool).start();
+	std::move(w1).get();
+	std::move(r1).get();
+
+	folly::coro::blockingWait([&]() -> folly::coro::Task<void> {
+		auto make_task = [&]() -> folly::coro::Task<int32_t> {
+			throw std::runtime_error("async exection");
+			co_return 100;
+		};
+		auto f = make_task().scheduleOn(&thread_pool).start();
+		try {
+			auto ret = co_await folly::coro::toTask(std::move(f));
+		} catch (const std::runtime_error& e) {
+			spdlog::error("{}", e.what());
+		}
+		co_return;
+	}());
+
+	folly::coro::blockingWait([&]() -> folly::coro::Task<void> {
+		spdlog::info("start test AsyncScope");
+		auto executor = co_await folly::coro::co_current_executor;
+		auto make_task = [&]() -> folly::coro::Task<> {
+			co_return;
+		};
+		folly::coro::AsyncScope scope;
+		for (int i = 0; i < 2; ++i)
+			scope.add(make_task().scheduleOn(executor));
+		co_await scope.joinAsync();
+		co_return;
+	}());
+
+	folly::coro::blockingWait([]() -> folly::coro::Task<void> {
+		auto [a, b, c] = co_await folly::coro::collectAllTry(
+			[]() -> folly::coro::Task<int> {
+				co_await folly::coro::sleep(1s);
+				spdlog::info("task1 thread id: {}", get_id());
+				co_return 42;
+			}(),
+			[]() -> folly::coro::Task<float> {
+				co_await folly::coro::sleep(2s);
+				spdlog::info("task2 thread id: {}", get_id());
+				co_return 3.14f;
+			}(),
+			[]() -> folly::coro::Task<void> {
+				spdlog::info("task3 thread id: {}", get_id());
+				co_await folly::coro::co_reschedule_on_current_executor;
+				spdlog::info("task3.1 thread id: {}", get_id());
+				throw std::runtime_error("error");
+			}());
+		spdlog::info("a: {}, b: {}, c: {}", a.hasValue(), b.hasValue(), c.hasException());
+	}());
+
+	auto thread_wheel_timekeeper_ptr = std::make_shared<folly::ThreadWheelTimekeeper>();
+	auto test_timeout = [&]() -> folly::coro::Task<> {
+		spdlog::info("test_timeout thread id: {}", get_id());
+		auto make_task = []() -> folly::coro::Task<int> {
+			co_await folly::coro::sleep(0s);
+			spdlog::info("timeout return 42");
+			co_return 42;
+		};
+		spdlog::info("start timeout");
+		auto result = co_await folly::coro::co_awaitTry(folly::coro::timeout(make_task(), 1s, thread_wheel_timekeeper_ptr.get()));
+		spdlog::info("has FutureTimeout Exception: {}", result.hasException<folly::FutureTimeout>());
+		spdlog::info("timeout thread id: {}", get_id());
+	};
+	auto t1 = test_timeout().scheduleOn(&thread_pool).start();
+	std::move(t1).get();
+
 	return 0;
 }
